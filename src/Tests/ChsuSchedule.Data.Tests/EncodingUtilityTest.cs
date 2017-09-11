840373b25f8057f03f7161e9a951da22fc08b573
mattche <chevychelovmatt@gmail.com>
2017-09-11 21:16:59 +0300

    Updated packages. Migrated to VS 2017 and PackageReference.

--- src/Tests/ChsuSchedule.Data.Tests/EncodingUtilityTest.cs
+++ src/Tests/ChsuSchedule.Data.Tests/EncodingUtilityTest.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using NUnit.Framework;
 
 namespace ChsuSchedule.Data.Html.Tests
 {
-	[TestFixture]
+    [TestFixture]
 	class EncodingUtilityTest : AssertionHelper
 	{
 		[Test]
